fb33902a4112b39aa624cff203ecccc345de5c3a
TDSUPPLY <terance111@qq.com>
2025-06-14 13:20:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ def export():
                      download_name='clockin_records.csv', as_attachment=True)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run()
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
