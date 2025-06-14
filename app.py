1561c6601d710bea44fbd8d782b338b265d55439
TDSUPPLY <terance111@qq.com>
2025-06-14 11:00:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def record_attendance():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # 使用 Render 提供的 PORT 环境变量
+    app.run(host='0.0.0.0', port=port)
