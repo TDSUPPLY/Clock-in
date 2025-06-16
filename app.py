8ac3dcfe2e9b23adb80ee1f17b820ba32d89c265
TDSUPPLY <terance111@qq.com>
2025-06-16 23:09:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("=DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
